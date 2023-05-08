e4fc022cfc05c64896ebd9a97fa1d99bd5f5ae90
Jonathan del Strother <me@delstrother.com>
2023-05-08 13:55:56 +0100

    Add devenv.nix for all the rails dependencies

---
+++ devenv.nix
@@ -0,0 +1,53 @@
+{ pkgs, lib, ... }:
+
+let
+  ruby = pkgs.ruby;
+in
+{
+  packages = [
+    ruby
+    pkgs.git
+    pkgs.cmake
+    pkgs.ncurses
+    pkgs.pkg-config
+    pkgs.ffmpeg
+    pkgs.openssl
+    pkgs.imagemagick
+    pkgs.libcxx
+    pkgs.libxml2
+    pkgs.libxslt
+    pkgs.libyaml
+    pkgs.mupdf
+    pkgs.mysql84
+    pkgs.poppler-utils
+    pkgs.readline
+    pkgs.sqlite
+    pkgs.postgresql
+    pkgs.yarn
+    pkgs.zlib
+    pkgs.zstd
+  ];
+
+  env = {
+    BUNDLE_BUILD__LIBXML___RUBY =
+      "--with-xml2-lib=${pkgs.libxml2.out}/lib --with-xml2-include=${pkgs.libxml2.dev}/include/libxml2"
+      + lib.optionals pkgs.stdenv.isDarwin " --with-iconv-dir=${pkgs.libiconv} --with-opt-include=${pkgs.libiconv}/include";
+
+    BUNDLE_BUILD__PG = "${builtins.concatStringsSep " " ruby.gems.pg.buildFlags}";
+
+    # Make sure that the regular user's environment doesn't collide with our nix gems
+    BUNDLE_PATH = "./.devenv/bundle";
+    GEM_HOME = "./.devenv/bundle/${ruby.rubyEngine}/${ruby.version.libDir}";
+    GEM_PATH = "./.devenv/bundle/${ruby.rubyEngine}/${ruby.version.libDir}";
+    BUNDLE_FORGET_CLI_OPTIONS = "true";
+
+    DYLD_LIBRARY_PATH = lib.makeLibraryPath [
+      pkgs.vips
+      pkgs.glib
+    ];
+    LD_LIBRARY_PATH = lib.makeLibraryPath [
+      pkgs.vips
+      pkgs.glib
+    ];
+  };
+}
